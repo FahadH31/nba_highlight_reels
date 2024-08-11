c572fc7f2ed19957513bab8b662ff668390600fd
FahadH31 <129327022+FahadH31@users.noreply.github.com>
2024-08-11 15:29:46 -0400

    Added Loop
    - Now able to download all clips as desired from user parameters.

--- nba.py
+++ nba.py
@@ -19,7 +19,6 @@ elif len(sys.argv) == 6:
     game_id = sys.argv[5]
     page_url = 'https://www.nba.com/stats/events?CFID=&CFPARAMS=&ContextMeasure='+context_measure+'&GameID='+game_id+'&PlayerID='+player_id+'&Season='+season+'&SeasonType='+season_type+'&TeamID=&flag=3&sct=plot&section=game'
 
-
 os.makedirs('clips', exist_ok=True) # Store clips in /clips
 
 # Open chrome browser and navigate to the appropriate URL.
@@ -27,26 +26,50 @@ browser = webdriver.Chrome()
 browser.get(page_url)
 time.sleep(5) # Wait for the page to load
 
-# Get the link to the clip
-try:
-    video_element = browser.find_element(By.ID,'vjs_video_3_html5_api')
-    video_url = video_element.get_attribute('src')
-except:
-    print('Element not found.')
+# Close the popup
+close_popup_element = browser.find_element(By.ID, 'onetrust-reject-all-handler')
+close_popup_element.click()
 
-# Name and download the clip
-count = 1
-output_file = f"clips/player_{player_id}_{context_measure}_{season}_{count}.mp4"
-try:
-    # Stream the download
-    with requests.get(video_url, stream=True) as r:
-        r.raise_for_status()  # Check if the request was successful
-        with open(output_file, 'wb') as f:
-            for chunk in r.iter_content(chunk_size=8192):  # 8 KB chunks
-                f.write(chunk)
-    print(f"Downloaded {video_url}")
-except requests.exceptions.HTTPError as err:
-    print("HTTP error occurred: {err}")
-except Exception as err:
-    print("An error occurred: {err}")
-count+=1
+# Scroll down by 800 pixels (to make the clips clickable)
+browser.execute_script("window.scrollBy(0, 800)")  
+next_videos = browser.find_elements(By.CLASS_NAME, 'Crom_stickyRank__aN66p') # Get list of all clips on the page
+
+# Initial counter values
+video_counter = 1 
+name_counter = 1
+
+# Loop to download all video clips on page.
+for x in next_videos:
+    
+    # Ensure correct number of loop iterations
+    if(video_counter == len(next_videos)):
+        break
+    
+    next_video = next_videos[video_counter] # Get the current clip
+    next_video.click() # Click on the current clip to start playing it
+
+    # Get the link to the clip
+    try:
+        video_element = browser.find_element(By.ID,'vjs_video_3_html5_api')
+        video_url = video_element.get_attribute('src')
+    except:
+        print('Element not found.')
+
+    # Name and download the clip
+    output_file = f"clips/player{player_id}_{context_measure}_{season}_{name_counter}.mp4"
+    try:
+        # Stream the download
+        with requests.get(video_url, stream=True) as r:
+            r.raise_for_status()  # Check if the request was successful
+            with open(output_file, 'wb') as f:
+                for chunk in r.iter_content(chunk_size=8192):  # 8 KB chunks
+                    f.write(chunk)
+        print(f"Downloaded {video_url}")
+    except requests.exceptions.HTTPError as err:
+        print("HTTP error occurred: {err}")
+    except Exception as err:
+        print("An error occurred: {err}")
+
+    name_counter+=1
+    video_counter+=1
+    time.sleep(2)
